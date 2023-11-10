de14a639f89ac24505c240c06371aaef95442872
meltphace7 <brd9326@protonmail.com>
2023-11-10 14:04:06 -0800

    mobile slider active class switches on item group in viewport

--- script.js
+++ script.js
@@ -122,31 +122,58 @@ productTabContainer.addEventListener("click", (event) => {
   }
 });
 
-const isInViewport = function (element) {
-  let rect = element.getBoundingClientRect();
-
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
   return (
     rect.top >= 0 &&
     rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
 
+const productItem = document.querySelector('.product-slider-item');
+
+isInViewport(productItem)
+
+let currentGroup = null;
 productSlider.addEventListener('scroll', (event) => {
-  console.log(productSlider.scrollLeft)
-  if (productSlider.scrollLeft < 800) {
-    console.log("GROUP 1")
+  if (window.screen.width > 492) return;
+   let scrollLeft = productSlider.scrollLeft;
+  let childWidth = productSlider.offsetWidth;
+  let newGroup;
+
+      console.log("scroll left", scrollLeft);
+      console.log("scroll width", childWidth);
+
+  if (scrollLeft < childWidth * 2.77) {
+    console.log('SECTION ONE')
+    newGroup = 1;
+  } else if(scrollLeft > childWidth * 2.77 && scrollLeft < childWidth * 5.56 ) {
+    console.log('SECTION TWO')
+    newGroup = 2;
+  } else {
+    console.log('SECTION THREE')
+    newGroup = 3;
+  }
+  if (currentGroup !== newGroup) {
+    // REMOVE ACTIVE CLASS FROM ALL TABS
+    document.querySelectorAll('.product-tab').forEach(tab => tab.classList.remove('product-tab--active'))
+    // ADD ACTIVE CLASS
+    const activeClass = document.querySelector(`.product-tab[data-tab="${newGroup}"]`);
+    activeClass.classList.add("product-tab--active");
+        lineSelector.style.width = `${
+          activeClass.offsetWidth - activeClass.offsetWidth / 2
+        }px`;
+        lineSelector.style.left = `${activeClass.offsetLeft}px`;
+    
   }
 
-  if (productSlider.scrollLeft > 800 && productSlider.scrollLeft < 2000) {
-    console.log("GROUP 2");
-  }
-
-   if (productSlider.scrollLeft > 2000) {
-     console.log("GROUP 3");
-   }
+  
 })
+
+
 
 // const productTabContainer = document.querySelector(".product-slider-tabs");
 // const productSlides = document.querySelectorAll(".product-collection");
